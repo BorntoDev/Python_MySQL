00b4dc694d22249c2b4590b37825e517f56d7afa
PrameKittikorn ✅ <100893808+PrameKittikorn@users.noreply.github.com>
2022-12-23 15:28:53 +0700

    Create 2_createdatabase.py

---
+++ 2_createdatabase.py
@@ -0,0 +1,13 @@
+import mysql.connector
+
+#ทำการเชื่อมต่อกับฐานข้อมูลง่าย ๆ แค่ใส่ Host / User / Password ให้ถูกต้อง
+connection = mysql.connector.connect(
+  host="localhost",
+  user="กรอก Username ตรงนี้",
+  password="กรอกรหัสผ่านตรงนี้"
+)
+
+db_cursor = connection.cursor()
+
+#เราสามารถรันคำสั่ง SQL ในการสร้าง Database  ได้เลย
+db_cursor.execute("CREATE DATABASE OurDB")
